80f5b5b1adce03508dd19b7d889e1d832da69456
Хохлова Наталия <nataliebasket@gmail.com>
2022-04-11 14:19:38 +0300

    верстка и стилизация макета

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+import gulp from 'gulp';
+import htmlmin from 'gulp-htmlmin';
+import browserSync from 'browser-sync';
+import plumber from 'gulp-plumber';
+import del from 'del';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import importScss from 'postcss-import';
+import postScss from 'postcss-scss';
+
+const {src, dest, watch, series, parallel} = gulp;
+
+export function html()  {
+  return src('./source/*.html')
+    .pipe(plumber())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('./public'));
+}
+
+export function copyImg () {
+  return src('./source/img/**/*.{png,jpg}')
+    .pipe(dest('public/img'));
+}
+
+
+export function styles () {
+  return src('./source/scss/*.scss', { sourcemaps: true})
+    .pipe(plumber())
+    .pipe(postcss([importScss()], { syntax: postScss }))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./public/styles', { sourcemaps: true}))
+    .pipe(browserSync.stream());
+}
+
+//Сервер
+export function server (done) {
+  browserSync.init({
+    server: {
+      baseDir: './public'
+    },
+    notify: false,
+    ui: false
+  });
+  done();
+}
+
+function reloadServer (done) {
+  browserSync.reload();
+  done();
+}
+
+function watcher () {
+  watch('./source/scss/**/*.scss', series(styles));
+  watch('./source/**/*.html', series(html, reloadServer));
+}
+
+export function copyOther () {
+  return src([
+    './source/fonts/*.{woff2,woff}',
+  ], {
+    base: './source'
+  })
+    .pipe(dest('./public'));
+}
+
+export function clear () {
+  return del('./public');
+}
+
+export default series (
+  clear,
+  parallel(
+    styles,
+    html,
+    copyImg,
+    copyOther
+  ),
+  series (
+    server,
+    watcher
+  )
+);
